feef96cd59bf32ff0e8a2bda836f3e5b246dfa65
Kuksa <kirkfanton.kb@gmail.com>
2022-10-26 17:11:59 +0300

    Debug. Add a random function

--- Program.cs
+++ Program.cs
@@ -9,16 +9,47 @@
     }
 }
 
+string RandomText()
+{   
+    Random res = new Random();
+    string alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    string text = "";
+    int textLenght = new Random().Next(2, 10);
+    int wordLenght = 0;
+    for (int i = 0; i < textLenght; i++)
+    {
+        wordLenght = new Random().Next(1, 10);
+        for (int j = 0; j < wordLenght; j++)
+        {
+            int symbol = res.Next(alphanumeric.Length);
+            text = text + alphanumeric[symbol];
+        }
+        if (i < textLenght - 1) text += " ";
+    }
+
+    return text;
+}
+
+bool YesOrNo(string answer)
+{
+    if (answer == "N" | answer == "n" | answer == "Y" | answer == "y") return true;
+    else return false;
+}
+
 string[] SortStringArray(string[] array)
 {
     int i = 0;
     string element = string.Empty;
     while (i < array.Length)
     {
-        if (array[i].Length > 3)
+        int wordLength = 0;
+        foreach (char c in array[i]) wordLength++;
+        if (wordLength > 3)
         {
             element = array[i];
-            array = Array.FindAll(array, e => e != element).ToArray();
+            int index = Array.IndexOf(array, element);
+            array = array.Where((e, j) => j != index).ToArray();
+            i--;
         }
         i++;
     }
@@ -28,21 +59,42 @@ string[] SortStringArray(string[] array)
 Console.WriteLine();
 Console.WriteLine("~Computer~ Program initial...");
 
-Console.WriteLine("~Computer~ Enter the elements of the future array " + Environment.NewLine
-                + "           separating them with a Space.");
-Console.WriteLine("~Computer~ Press Enter to finish typing.");
-Console.Write("~Computer~ Enter any text: ");
-string text = Console.ReadLine()!;
+Console.WriteLine("~Computer~ Do you want create a random text?");
+Console.Write("~Human~ Y/N: ");
+bool check = false;
+string answer = string.Empty;
+while (!check)
+{
+    int i = 0;
+    if (i > 0)
+    {
+        Console.Write("~Computer~ Enter \"N\" or \"Y\": ");
+    }
+    answer = Console.ReadLine()!;
+    check = YesOrNo(answer);
+}
+
+string text = string.Empty;
+if (answer == "n" | answer == "N")
+{
+    Console.WriteLine("~Computer~ Enter the elements of the future array " + Environment.NewLine
+                    + "           separating them with a Space.");
+    Console.WriteLine("~Computer~ Press Enter to finish typing.");
+    Console.Write("~Human~ Enter any text: ");
+    text = Console.ReadLine()!;
+} else text = RandomText();
+
+
 Console.WriteLine("~Computer~ Creating a string array...");
 Console.WriteLine();
 
 string[] textArray = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-Console.WriteLine("~Computer~ Your string array: ");
+Console.Write("~Computer~ Your string array: ");
 PrintStringArray(textArray);
 Console.WriteLine();
 
 textArray = SortStringArray(textArray);
-Console.WriteLine("~Computer~ Your sorted array: ");
+Console.Write("~Computer~ Your sorted array: ");
 PrintStringArray(textArray);
 Console.WriteLine();
 
